af4dd0174785aa4e5765ceb1e9b01c419f41c9f8
Katrina Owen <katrina.owen@gmail.com>
2015-10-12 12:10:59 -0600

    Simplify days in month computation

--- month.go
+++ month.go
@@ -3,6 +3,8 @@
 // ability to return the last day of any given month.
 package month
 
+import "time"
+
 // Month represents a standard calendar month, applicable to integer format.
 type Month int
 
@@ -37,20 +39,6 @@ var monthNames = [12]string{
 	"December",
 }
 
-var monthDays = map[int]int{
-	1:  31,
-	3:  31,
-	4:  30,
-	5:  31,
-	6:  30,
-	7:  31,
-	8:  31,
-	9:  30,
-	10: 31,
-	11: 30,
-	12: 31,
-}
-
 // String returns an English language based representation of m. E.g., January,
 // February, etc.
 func (m Month) String() string {
@@ -61,16 +49,6 @@ func (m Month) String() string {
 // LastDay returns the last day of m, based on year. The function takes into
 // account leap years.
 func (m Month) LastDay(year uint16) int {
-
-	if m == 2 {
-
-		isLeapYear := year%4 == 0 &&
-			(year%100 != 0 || year%400 == 0)
-
-		if isLeapYear {
-			return 29
-		}
-		return 28
-	}
-	return monthDays[int(m)]
+	ts := time.Date(int(year), time.Month(m)+1, 0, 0, 0, 0, 0, time.UTC)
+	return ts.Day()
 }
